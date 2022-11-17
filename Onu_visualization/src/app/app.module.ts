7264e695a3991bdb4970b178699d4fe68793f38c
Renatto Padilha <rspf@cin.ufpe.br>
2022-11-17 16:08:57 -0300

    Adding Filter Button Component

--- Onu_visualization/src/app/app.module.ts
+++ Onu_visualization/src/app/app.module.ts
@@ -3,11 +3,13 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FilterButtonComponent } from './filter-button/filter-button.component';
 import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FilterButtonComponent
   ],
   imports: [
     BrowserModule,
---
+++ Onu_visualization/src/app/filter-button/filter-button.component.ts
@@ -0,0 +1,36 @@
+import { Component, OnInit } from '@angular/core';
+import { faCircleChevronDown } from '@fortawesome/free-solid-svg-icons';
+
+@Component({
+  selector: 'app-filter-button',
+  templateUrl: './filter-button.component.html',
+  styleUrls: ['./filter-button.component.css'],
+})
+export class FilterButtonComponent implements OnInit {
+  //div size
+  width = '15vw';
+  height = '5vh';
+  //icon position
+  icon = faCircleChevronDown;
+  iconPosition = 'translateY(-50%) rotate(0deg)';
+  //filter window variable
+  isFilterOpen = false;
+
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  //Change icon position and open window
+  buttonClick() {
+    if (this.isFilterOpen) {
+      //Close Filter Window
+      this.isFilterOpen = false;
+      this.iconPosition = 'translateY(-50%) rotate(0deg)';
+    } else {
+      //Open Filter Window
+      this.isFilterOpen = true;
+      this.iconPosition = 'translateY(-50%) rotate(-180deg)';
+    }
+    console.log(this.iconPosition);
+  }
+}
